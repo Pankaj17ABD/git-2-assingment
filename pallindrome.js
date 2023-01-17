0fc7546dc47066211896fc2144574f90b5fd9770
Shadman Khan <shaad2209@gmail.com>
2023-01-17 14:22:24 +0530

    pallindrome

---
+++ pallindrome.js
@@ -0,0 +1,16 @@
+let str="naman";
+
+let rev=""
+
+for(let i=str.length-1 i>=0; i--) 
+{
+  rev=rev+str[i]; 
+}
+
+if(str==rev) {
+console.log("it is a pallindrome")
+}
+
+else {
+console.log("it is not pallindrome")
+}
